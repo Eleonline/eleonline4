3125df65d60e040757f89ec86f85b8759a2dee3a
Alexc-1 <Suffer_it@yahoo.it>
2026-01-10 20:12:05 +0100

    graifca importa lista

--- admin/modules/importa_lista.php
+++ admin/modules/importa_lista.php
@@ -1,9 +1,14 @@
 <?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 require_once '../includes/check_access.php';
 
-$param=strtolower($_SERVER['REQUEST_METHOD']) == 'get' ? $_GET : $_POST;
-$id_cons_gen=intval($_SESSION['id_cons_gen']);
-if (isset($param['datafile'])) $datafile=addslashes($param['datafile']); else $datafile='';
+// Parametri
+$param = strtolower($_SERVER['REQUEST_METHOD']) == 'get' ? $_GET : $_POST;
+$id_cons_gen = intval($_SESSION['id_cons_gen']);
+$id_comune = intval($_SESSION['id_comune']); // presupposto
+if (isset($param['datafile'])) $datafile = addslashes($param['datafile']); else $datafile='';
 
 function insgruppo()
 {
@@ -149,149 +154,85 @@ function inscandi()
 	}
 }
 
+// Gestione upload
+if (!isset($_FILES['datafile']['tmp_name']) || !is_uploaded_file($_FILES['datafile']['tmp_name'])) {
+    // Mostra form
+    include('importa_lista_2.php');
+    exit;
+} else {
+    $idcns = $id_cons_gen; 
 
+    // Pulizia dati vecchi
+    $tables = ['ele_voti_ref','ele_voti_candidato','ele_voti_lista','ele_voti_gruppo','ele_voti_parziale','ele_candidato','ele_lista','ele_gruppo'];
+    foreach($tables as $tbl){
+        $sql = "DELETE FROM ".$prefix."_".$tbl." WHERE id_cons=$idcns";
+        $res_del = $dbi->prepare($sql);
+        $res_del->execute();
+    }
+    // Reset sezioni
+    $sql = "UPDATE ".$prefix."_ele_sezione SET validi='0', contestati='0', validi_lista='0', nulli='0', bianchi='0', contestati_lista='0', voti_nulli_lista='0' WHERE id_cons=$idcns";
+    $res_del = $dbi->prepare($sql);
+    $res_del->execute();
 
+    // Lettura file
+    $datafile = $_FILES['datafile']['tmp_name'];
+    $arrFile = file($datafile);
+    $handle = fopen($datafile, "r");
 
-
-$sql="SELECT t1.id_cons, t2.descrizione FROM ".$prefix."_ele_cons_comune as t1 left join ".$prefix."_ele_consultazione as t2 on t1.id_cons_gen=t2.id_cons_gen where t1.id_comune='$id_comune' and t2.id_cons_gen='$id_cons_gen'";
-$res = $dbi->prepare("$sql");
-$res->execute();	
-list($id_cons,$descrizione) = $res->fetch(PDO::FETCH_NUM);
-if (!isset($_FILES['datafile']['tmp_name']) or !is_uploaded_file($_FILES['datafile']['tmp_name'])) 
-{
-	if (isset($_GET['help'])) $help=intval($_GET['help']);
-   	global $help,$language;
-	if (isset($help)) include("language/$language/ele_importa.html");
-	include('importa_html.php');
-}else{
-	$idcns=$id_cons; 
-	$sql="delete from ".$prefix."_ele_voti_ref where id_cons=$idcns";
-	$res_del = $dbi->prepare("$sql");
-	$res_del->execute();	
-	$sql="delete from ".$prefix."_ele_voti_candidato where id_cons=$idcns";
-	$res_del = $dbi->prepare("$sql");
-	$res_del->execute();	
-	$sql="delete from ".$prefix."_ele_voti_lista where id_cons=$idcns";
-	$res_del = $dbi->prepare("$sql");
-	$res_del->execute();	
-	$sql="delete from ".$prefix."_ele_voti_gruppo where id_cons=$idcns";
-	$res_del = $dbi->prepare("$sql");
-	$res_del->execute();	
-	$sql="update ".$prefix."_ele_sezione set validi='0', contestati='0', validi_lista='0', nulli='0',bianchi='0',contestati_lista='0', voti_nulli_lista='0'  where id_cons=$idcns";
-	$res_del = $dbi->prepare("$sql");
-	$res_del->execute();	
-	$sql="delete from ".$prefix."_ele_voti_parziale where id_cons=$idcns";
-	$res_del = $dbi->prepare("$sql");
-	$res_del->execute();	
-	$sql="delete from ".$prefix."_ele_candidato where id_cons=$idcns";
-	$res_del = $dbi->prepare("$sql");
-	$res_del->execute();	
-	$sql="delete from ".$prefix."_ele_lista where id_cons=$idcns";
-	$res_del = $dbi->prepare("$sql");
-	$res_del->execute();	
-	$sql="delete from ".$prefix."_ele_gruppo where id_cons=$idcns";
-	$res_del = $dbi->prepare("$sql");
-	$res_del->execute();	
-	$datafile=$_FILES['datafile']['tmp_name'];
-	$arrFile = file($datafile);
-	$handle = fopen($datafile, "r");
-	$test=array();
-	$errore=0;
-	$fine=0;
-	$numgruppo=0;
-	$numlista=0;
-// Set counters
+    // Variabili di gestione
     $currentLine = 0;
     $cntFile = count($arrFile);
-	$tabs=array($prefix."_ele_gruppo",$prefix."_ele_lista",$prefix."_ele_candidato",$prefix."_ele_circoscrizione");
-	$x=0;$k=0;
-	$scarto=0;
-	$primog=0;
-	$primol=0;
-	$conta=array();
-	$currentLine = 0;
-	$x=0;$k=0;
-	$y=0;
-	$ar_gruppo=array(array());
-	$ar_lista=array(array());
-	$ar_candi=array(array());
-	$z=0;
-	$tab=substr($arrFile[$currentLine],1,-2);
-	$conf=$tabs[$x];
-	if($k==0) {while (substr($arrFile[$currentLine],1,-2)!=$conf and $currentLine <= $cntFile) $currentLine++; $k++;}
-	$currentLine++;
-	while($currentLine <= $cntFile and $fine==0){
-		if(isset($arrFile[$currentLine])){ 
-			$appo=substr($arrFile[$currentLine],1,-2);
-			if($appo==$prefix."_ele_candidati") $appo=$prefix."_ele_candidato";
-		}else $appo='';
-		if (isset($tabs[($x+1)]) and $appo==$tabs[($x+1)]){ $x++;$conf=$tabs[$x];$currentLine++; continue;}
-		$test=explode(':',$appo);
-		if(!is_array($test)) {die("errore di import<br>");}
-		foreach($test as $key=>$val) { 
-			if ($conf==$prefix."_ele_gruppo"){
-				$ar_gruppo[$z][$key]=addslashes(base64_decode($val));
-			}
-			elseif ($conf==$prefix."_ele_lista"){
-				if($primog==0){
-					$gruppofil= array_filter($ar_gruppo);
-					$numgruppo=count($gruppofil);
-					insgruppo();
-					$primog=1;
-					unset($ar_gruppo);
-				}
-				$ar_lista[$z][$key]=addslashes(base64_decode($val));
-			}
-			elseif ($conf==$prefix."_ele_candidato" or $conf==$prefix."_ele_candidati"){ 
-				if($primog==0){
-					$gruppofil= array_filter($ar_gruppo);
-					$numgruppo=count($gruppofil);
-					insgruppo();
-					$primog=1;
-					unset($ar_gruppo);
-				}
-				elseif($primol==0){
-					$listafil= array_filter($ar_lista);
-					$numlista=count($listafil);
-					inslista();
-					$primol=1;
-					unset($ar_lista);
-				}
-				$ar_candi[$z][$key]=addslashes(base64_decode($val));
-			}
-			elseif ($conf==$prefix."_ele_circoscrizione"){
-				if($primog==0){
-					$gruppofil= array_filter($ar_gruppo);
-					$numgruppo=count($gruppofil);
-					insgruppo();
-					$primog=1;
-					unset($ar_gruppo);
-				}
-				elseif($primol==0){
-					$listafil= array_filter($ar_lista);
-					$numlista=count($listafil);
-					inslista();
-					$primol=1;
-					unset($ar_lista);
-				}else{
-					inscandi();
-					unset($ar_candi);
-					$fine=1;
-					break;
-				}
-	}}
-			$currentLine++;
-			$z++;
-	}
-	fclose($handle);
-	if ($numgruppo){
-		Header("Location: modules.php?op=27&id_cons_gen=$id_cons_gen");
-	}
-	elseif ($numlista) {
-		Header("Location: modules.php?op=28&id_cons_gen=$id_cons_gen");
-	}
-	else Header("Location: modules.php?op=28&id_cons_gen=$id_cons_gen");
+    $tabs = array($prefix."_ele_gruppo",$prefix."_ele_lista",$prefix."_ele_candidato",$prefix."_ele_circoscrizione");
+    $x = $k = $y = $z = 0;
+    $primog = $primol = 0;
+    $ar_gruppo = array(array());
+    $ar_lista = array(array());
+    $ar_candi = array(array());
+    $tab = substr($arrFile[$currentLine],1,-2);
+    $conf = $tabs[$x];
 
+    // Scansione file
+    if($k==0){
+        while (substr($arrFile[$currentLine],1,-2)!=$conf && $currentLine <= $cntFile) $currentLine++; 
+        $k++;
+    }
+    $currentLine++;
+
+    $fine = 0;
+    while($currentLine <= $cntFile && $fine==0){
+        if(isset($arrFile[$currentLine])){ 
+            $appo = substr($arrFile[$currentLine],1,-2);
+            if($appo==$prefix."_ele_candidati") $appo=$prefix."_ele_candidato";
+        }else $appo='';
+
+        if (isset($tabs[($x+1)]) && $appo==$tabs[($x+1)]){ $x++; $conf=$tabs[$x]; $currentLine++; continue; }
+
+        $test = explode(':',$appo);
+        if(!is_array($test)) die("Errore di import<br>");
+
+        foreach($test as $key=>$val){
+            if($conf==$prefix."_ele_gruppo") $ar_gruppo[$z][$key]=addslashes(base64_decode($val));
+            elseif($conf==$prefix."_ele_lista"){
+                if($primog==0){ insgruppo(); $primog=1; unset($ar_gruppo); }
+                $ar_lista[$z][$key]=addslashes(base64_decode($val));
+            }
+            elseif($conf==$prefix."_ele_candidato"){ 
+                if($primog==0){ insgruppo(); $primog=1; unset($ar_gruppo); }
+                elseif($primol==0){ inslista(); $primol=1; unset($ar_lista); }
+                $ar_candi[$z][$key]=addslashes(base64_decode($val));
+            }
+            elseif($conf==$prefix."_ele_circoscrizione"){
+                if($primog==0){ insgruppo(); $primog=1; unset($ar_gruppo); }
+                elseif($primol==0){ inslista(); $primol=1; unset($ar_lista); }
+                inscandi(); unset($ar_candi); $fine=1; break;
+            }
+        }
+        $currentLine++; $z++;
+    }
+    fclose($handle);
+
+    // Redireziona
+    if (!empty($ar_gruppo)) header("Location: modules.php?op=27&id_cons_gen=$id_cons_gen");
+    else header("Location: modules.php?op=28&id_cons_gen=$id_cons_gen");
 }
-
 ?>
